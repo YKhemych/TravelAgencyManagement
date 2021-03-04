2eb9cf64aba468a570845bea98bd369ade100bfe
Yosyp <yosyp0108khemych@gmail.com>
2021-03-04 23:07:50 +0200

    Import user and auth modules

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,11 +1,13 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './controllers/app.controller';
 import { AppService } from './services/app.service';
-import {DatabaseModule} from "./modules/database.module";
+import { DatabaseModule } from './modules/database.module';
+import { UserModule } from './modules/user.module';
+import { AuthModule } from './modules/auth.module';
 
 @Module({
-  imports: [DatabaseModule],
+  imports: [DatabaseModule, UserModule, AuthModule],
   controllers: [AppController],
-  providers: [AppService],
+  providers: [AppService]
 })
 export class AppModule {}
