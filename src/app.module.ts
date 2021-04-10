d27c235262ef9f0870d6aa463a5dc54943cbb7ba
Yosyp <yosyp0108khemych@gmail.com>
2021-04-10 13:51:25 +0300

    Add company module

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,9 +4,10 @@ import { AppService } from './services/app.service';
 import { DatabaseModule } from './modules/database.module';
 import { UserModule } from './modules/user.module';
 import { AuthModule } from './modules/auth.module';
+import { CompanyModule } from './modules/company.module';
 
 @Module({
-  imports: [DatabaseModule, UserModule, AuthModule],
+  imports: [DatabaseModule, UserModule, AuthModule, CompanyModule],
   controllers: [AppController],
   providers: [AppService]
 })
