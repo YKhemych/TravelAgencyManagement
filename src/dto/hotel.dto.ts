ab46ab5e9d3da3b3b0ac9505eb4c9de830f34d4f
Yosyp <yosyp0108khemych@gmail.com>
2021-05-03 22:41:15 +0300

    Add hotel dto

---
+++ src/dto/hotel.dto.ts
@@ -0,0 +1,46 @@
+import { ApiProperty } from '@nestjs/swagger';
+import { IsNumber, IsOptional, IsPhoneNumber, IsString, ValidateNested } from 'class-validator';
+import { Type } from 'class-transformer';
+import { AddressDto } from './address.dto';
+
+export class HotelDto {
+    id: number;
+
+    @ApiProperty({ example: 'Hotel name' })
+    @IsString()
+    name: string;
+
+    @ApiProperty({ example: 'description' })
+    @IsString()
+    description: string;
+
+    @ApiProperty({ example: '380977913642' })
+    @IsPhoneNumber()
+    phone: string;
+
+    @ApiProperty({ example: 5 })
+    @IsNumber()
+    @IsOptional()
+    rating?: number;
+
+    @ApiProperty({ type: AddressDto })
+    @Type(() => AddressDto)
+    @ValidateNested()
+    address: AddressDto;
+
+    companyId: number;
+}
+
+export class HotelDataDto {
+    @ApiProperty({ type: HotelDto })
+    @Type(() => HotelDto)
+    @ValidateNested()
+    data: HotelDto;
+}
+
+export class HotelArrayDataDto {
+    @ApiProperty({ type: [HotelDto] })
+    @Type(() => HotelDto)
+    @ValidateNested({ each: true })
+    data: HotelDto[];
+}
