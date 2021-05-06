831926ad7eb2e5892c91ab3798736f610861dc55
Yosyp <yosyp0108khemych@gmail.com>
2021-05-07 00:17:54 +0300

    Add room controller and service

--- src/modules/hotel.module.ts
+++ src/modules/hotel.module.ts
@@ -5,10 +5,12 @@ import { DatabaseModule } from './database.module';
 import { AddressService } from '../services/address.service';
 import { HotelService } from "../services/hotel.service";
 import { HotelController } from "../controllers/hotel.controller";
+import { RoomService } from "../services/room.service";
+import { RoomController } from "../controllers/room.controller";
 
 @Module({
   imports: [DatabaseModule, AuthModule, DatabaseModelsModule],
-  controllers: [HotelController],
-  providers: [HotelService, AddressService]
+  controllers: [HotelController, RoomController],
+  providers: [HotelService, AddressService, RoomService]
 })
 export class HotelModule {}
