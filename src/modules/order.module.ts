0cad3b19211ecffe79797325e09cefa7fb872099
Yosyp <yosyp0108khemych@gmail.com>
2021-05-10 21:24:23 +0300

    Add order module

---
+++ src/modules/order.module.ts
@@ -0,0 +1,13 @@
+import { Module } from '@nestjs/common';
+import { AuthModule } from './auth.module';
+import { DatabaseModelsModule } from './databaseModels.module';
+import { DatabaseModule } from './database.module';
+import { OrderController } from "../controllers/order.controller";
+import { OrderService } from "../services/order.service";
+
+@Module({
+  imports: [DatabaseModule, AuthModule, DatabaseModelsModule],
+  controllers: [OrderController],
+  providers: [OrderService]
+})
+export class OrderModule {}
